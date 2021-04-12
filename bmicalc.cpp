7d0281a78da993c47bf1f6d77a05323ec85f8773
Brian-wKicheu <78160253+Brian-wKicheu@users.noreply.github.com>
2021-04-12 17:42:28 +0300

    Update bmicalc.cpp

--- bmicalc.cpp
+++ bmicalc.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main(){
+    // this program calculates bmi and checks if is less 18.5 is underweigth, if greater than 25 is overweigth,
+    // and if is btwn 18.5 and 25 is normal
     float weight,height, bmi;
     weight = 65, height = 1.6;
     bmi = weight/(height * height);
